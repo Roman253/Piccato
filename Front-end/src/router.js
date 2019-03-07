e1772e8f59d5b615ffc6ea8df35e9aeb1d7f5c13
Åsa H <asa.hildingsson@hotmail.com>
2019-03-07 10:52:17 +0100

    Creating views structure

--- Front-end/src/router.js
+++ Front-end/src/router.js
@@ -5,19 +5,41 @@ import Home from './views/Home.vue'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
-  ]
+    routes: [{
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/admin',
+            name: 'admin',
+            component: () =>
+                import ('./views/Admin.vue')
+        },
+        {
+            path: '/booking',
+            name: 'booking',
+            component: () =>
+                import ('./views/Booking.vue')
+        },
+        {
+            path: '/calendar',
+            name: 'calendar',
+            component: () =>
+                import ('./views/Calendar.vue')
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: () =>
+                import ('./views/Login.vue')
+        },
+        {
+            path: '/user',
+            name: 'user',
+            component: () =>
+                import ('./views/User.vue')
+        },
+
+    ]
 })
---
+++ Front-end/src/views/User.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="user">
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/User.vue'
+
+export default {
+  name: 'user',
+  components: {
+    user
+  }
+}
+</script>
