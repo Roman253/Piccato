f74ec8cd3b54eddd705920d87d044a644e7e6bf2
Åsa H <asa.hildingsson@hotmail.com>
2019-03-10 20:08:07 +0100

    Added Calendar + disable choosing dates pre-today

--- Front-end/src/main.js
+++ Front-end/src/main.js
@@ -6,7 +6,18 @@ import store from './store'
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
+
+
+import VCalendar from 'v-calendar';
+import 'v-calendar/lib/v-calendar.min.css';
+
+// Use v-calendar, v-date-picker & v-popover components
+Vue.use(VCalendar, {
+    firstDayOfWeek: 2,
+    popoverVisibility: "visible",
+
+});
--- Front-end/src/views/Calendar.vue
+++ Front-end/src/views/Calendar.vue
@@ -1,19 +1,48 @@
 <template>
-      <div class="calendar">
+      <div class="maincalendar">
+
+
 
 <h2> Choose dates </h2>
 
-<p>Insert Vue calendar here </p>
-  </div>
+<div class="calendar">
 
+ <v-date-picker mode='range' v-model='selectedDate' :min-date='new Date()' show-caps>
+</v-date-picker>
+
+  </div>
+</div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      selectedValue: new Date(),
 
-}
+      selectedDate: {
+        start: new Date(), 
+        end: new Date()    
+      }    };
+  },
+};
 </script>
 
-<style>
+<style lang="scss">
+@import '../scss/main.scss';
+
+.calendar {
+margin: 0 auto;
+
+}
+
+
+
+.popover-container[data-v-1ad2436f] {
+    width: 17%;
+    margin: 0 auto;
+    
+    }
+
 
 </style>
