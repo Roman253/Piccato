6372bb3fd09dfa2a27ceb5346e6f076c72f94e2f
Åsa H <asa.hildingsson@hotmail.com>
2019-03-11 13:43:07 +0100

    Update booking.js

--- Back-end/models/booking.js
+++ Back-end/models/booking.js
@@ -4,10 +4,14 @@ let Schema = mongoose.Schema;
 // Create schema
 let bookingSchema = new Schema({
     artwork: Object,
-    
+    user: Object,
+    booking: {
+        from: String,
+        to: String
+    }
 });
 
 // A model based on the schema
-let Booking= mongoose.model('booking', bookingSchema);
+let Booking = mongoose.model('booking', bookingSchema);
 
 module.exports = Booking;
