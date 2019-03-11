057c1021a0202f853fe6b4213d52745bb558ab75
Christian <32764514+neowara@users.noreply.github.com>
2019-03-11 02:00:56 +0100

    Installed express-validator as a package dependency. It's used to check if all forms are filled and if they are filled correctly that way the user registration process will go smoothly. Created a basic post request on the registration route that works well and posts to the 'users' collection in our database. Next step: password encryption and user login

--- Back-end/index.js
+++ Back-end/index.js
@@ -2,10 +2,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const expressValidator = require('express-validator');
 
 let app = express();
 app.use(express.json());
 app.use(cors());
+app.use(expressValidator());
 
 
 // Connect to our DB 
@@ -18,10 +20,12 @@ mongoose.connect(`mongodb+srv://Admin1:${process.env.PASSWORD}@cluster0-y175r.mo
     });
 
 
-app.listen(3000, () => {
-    console.info('Server is running: 3000.')
-});
-
 //Routes 
 let users = require('./routes/users');
-app.use('/users', users);
+
+app.route('/register')
+.post(users.post)
+
+app.listen(3000, () => {
+    console.info('Server is running: using port 3000.')
+});
--- Back-end/routes/users.js
+++ Back-end/routes/users.js
@@ -1,9 +1,30 @@
 const express = require('express');
-const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
 
 //Get our User Model 
 let User = require('../models/user');
 
+module.exports.post = async (req, res) => {
 
+    try {
 
-module.exports = router;
+        check('name', 'Name is required').isEmpty();
+        check('email', 'Email is required').isEmpty();
+        check('email', 'Email is not valid').isEmail();
+        check('lastname', 'Last Name is required').isEmpty();
+        check('password', 'Password is required').isEmpty();
+
+        let resp = await User.create({
+            name: req.body.name,
+            lastname: req.body.lastname,
+            email: req.body.email,
+            password: req.body.password
+        });
+        res.status(200).send(resp);
+
+    } catch (errors) {
+        console.log(errors.message);
+        res.status(400).send(errors.message);
+    }
+
+};
