1fbe6af2d145a2cdc4dd2402bc3963a1ce5a021b
Åsa H <asa.hildingsson@hotmail.com>
2019-03-11 12:07:06 +0100

    calendar, design, admin form

--- Back-end/models/artwork.js
+++ Back-end/models/artwork.js
@@ -4,19 +4,10 @@ let Schema = mongoose.Schema;
 /* How our DB should collect data and also needed in our vue/admin site so we send the correct data */
 let artworkSchema = new Schema({
     title: String,
-    artist: String, 
+    artist: String,
     description: String,
     price: Number,
-    when: {
-        date: String,
-        start: String,
-        end: String,
-    },
-    where: {
-        location: String,
-        adress: String,
-    },
-    spot: String,
+    artwork: String
 });
 
 let Artwork = mongoose.model('artwork', artworkSchema);
