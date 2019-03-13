3b2c6d946b08b6f98568707997d800353e9cccb2
asayehla <asa@palholmen.se>
2019-03-14 00:26:40 +0100

    trying to make routerview links

---
+++ Front-end/src/components/artworkDesc.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="media-body">
+    <h3 class="media-heading">Title: {{artwork.title}}</h3>
+    <p class="media-p">
+      Artist: {{artwork.artist}}
+      <br>
+      Price: {{artwork.price}}:-
+      <br>
+      Description: {{artwork.description}}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'desc',
+    computed: {
+        desc() {
+            return this.$store.getters.getDescById(this.$route.params.descId);
+        }
+    }
+};
+</script>
+
+<style>
+.media-body {
+  flex: 1;
+  background: red;
+}
+.media-heading {
+  margin: 0 0 0.5em;
+}
+.media-p {
+  max-width: 100%;
+}
+</style>
--- Front-end/src/router.js
+++ Front-end/src/router.js
@@ -6,40 +6,57 @@ Vue.use(Router)
 
 export default new Router({
     routes: [{
-            path: '/',
-            name: 'home',
-            component: Home
-        },
-        {
-            path: '/admin',
-            name: 'admin',
-            component: () =>
-                import ('./views/Admin.vue')
-        },
-        {
-            path: '/booking',
-            name: 'booking',
-            component: () =>
-                import ('./views/Booking.vue')
-        },
-        {
-            path: '/calendar',
-            name: 'calendar',
-            component: () =>
-                import ('./views/Calendar.vue')
-        },
-        {
-            path: '/login',
-            name: 'login',
-            component: () =>
-                import ('./views/Login.vue')
-        },
-        {
-            path: '/user',
-            name: 'user',
-            component: () =>
-                import ('./views/User.vue')
-        }
+        path: '/',
+        name: 'home',
+        component: Home
+    },
+    {
+        path: '/admin',
+        name: 'admin',
+        component: () =>
+            import('./views/Admin.vue')
+    },
+    {
+        path: '/booking',
+        name: 'booking',
+        component: () => import('./views/Booking.vue'),
+        children:
+            [
+                {
+                    path: 'artworkItem/:id',
+                    name: 'artworkItem',
+                    component: () => import('./components/artworkItem.vue'),
+                    children:
+                        [
+                            {
+                                path: 'artworkDesc/:artworkDescId',
+                                name: 'artworkDesc',
+                                component: () => import('./components/artworkDesc.vue')
+                            }
+                        ]
+
+                }
+
+            ]
+    },
+    {
+        path: '/calendar',
+        name: 'calendar',
+        component: () =>
+            import('./views/Calendar.vue')
+    },
+    {
+        path: '/login',
+        name: 'login',
+        component: () =>
+            import('./views/Login.vue')
+    },
+    {
+        path: '/user',
+        name: 'user',
+        component: () =>
+            import('./views/User.vue')
+    }
 
     ]
 })
