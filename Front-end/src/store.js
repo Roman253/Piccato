b7481b0b78f0ebb6d37faf806ecc323fc9e930a3
Åsa H <asa.hildingsson@hotmail.com>
2019-03-19 13:27:00 +0100

    Update store.js

--- Front-end/src/store.js
+++ Front-end/src/store.js
@@ -66,6 +66,16 @@ export default new Vuex.Store({
             await Axios.delete(`http://localhost:3000/artworks/${id._id}`);
         },
 
+        async sendEditArtwork(ctx, artwork) {
+            try {
+                await Axios.put(`http://localhost:3000/artworks/`, artwork);
+                ctx.dispatch("getArtworks");
+            } catch (err) {
+                // eslint-disable-next-line no-console
+                console.err(err.stack);
+            }
+        },
+
 
         //get artwork from the API
         async getArtworks(ctx) {
